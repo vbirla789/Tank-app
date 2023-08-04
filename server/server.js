8b5fcaeafefe38ed16c90090940e368dffeae6cf
VIIBII <birlavishal90@gmail.com>
2023-08-05 03:36:37 +0530

    ss

--- server/server.js
+++ server/server.js
@@ -19,7 +19,7 @@ export const instance = new Razorpay({
 });
 
 const server = app.listen(process.env.PORT, () =>
-  console.log(`Server is working on ${process.env.PORT}`)
+  console.log(`Server is working on ${process.env.PORT || 10000}`)
 );
 
 // Unhandled promise rejection
