767e221382b07d323a463b6a906e7671b09bc0ce
VIIBII <birlavishal90@gmail.com>
2023-08-08 19:27:45 +0530

    path added

--- server/utils/jwtToken.js
+++ server/utils/jwtToken.js
@@ -12,7 +12,8 @@ export const sendToken = (user, statusCode, res) => {
         Date.now() + process.env.COOKIE_EXPIRE * 24 * 60 * 60 * 1000
       ),
       httpOnly: true,
-      domain: ".watertankdoctor.com",
+      domain: "www.watertankdoctor.com", // Set the domain
+      path: "/", // Set the path to '/'
     };
 
     res.status(statusCode).cookie("token", token, options).json({
