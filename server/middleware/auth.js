5b8d7e26b2bbbe5f5cde3a3f7b9db70b70105c14
VIIBII <birlavishal90@gmail.com>
2023-08-09 11:22:59 +0530

    ddd

--- server/middleware/auth.js
+++ server/middleware/auth.js
@@ -4,7 +4,7 @@ import jwt from "jsonwebtoken";
 import { User } from "../models/userModels.js";
 
 export const isAuthenticatedUser = asyncError(async (req, res, next) => {
-  const { token } = req.cookie;
+  const { token } = req.cookies;
 
   if (!token) {
     return next(new ErrorHandler("Please login to access this resource", 401));
