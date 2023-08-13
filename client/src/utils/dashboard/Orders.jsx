4df9d2c0a775990367e8a88241a5da0154f41049
VIIBII <birlavishal90@gmail.com>
2023-08-13 22:04:42 +0530

    loader added

--- client/src/utils/dashboard/Orders.jsx
+++ client/src/utils/dashboard/Orders.jsx
@@ -4,34 +4,49 @@ import { getAllOrders } from "../../redux/Action/orderAction";
 import { useDispatch, useSelector } from "react-redux";
 import { clearErrors } from "../../redux/Action/orderAction";
 import OrderData from "./OrderData";
+import DotLoader from "react-spinners/DotLoader";
 
 const Orders = () => {
   const dispatch = useDispatch();
 
   const { error, orders } = useSelector((state) => state.allOrders);
 
-  console.log(orders);
+  // console.log(orders);
+  const [loading, setLoading] = useState(false);
 
   useEffect(() => {
     dispatch(getAllOrders());
+    setTimeout(() => {
+      setLoading(false);
+    }, 2000);
   }, [dispatch]);
 
   return (
     <div className="grid grid-cols-6 gap-4 min-h-[100vh] pt-[15vh]">
       <Sidebar />
-      <div className="col-span-5 px-10">
-        <h1 className="text-3xl font-semibold text-center mb-5">Orders</h1>
-        <div className="flex justify-around items-center mb-5 bg-slate-300 px-3 rounded-3xl py-3 font-semibold">
-          <span>Order ID</span>
-          <h1>Status</h1>
-          <p>Item Name</p>
-          <p>Amount</p>
-          <p>Actions</p>
-        </div>
+      {loading ? (
+        <DotLoader
+          color={"#142D43"}
+          loading={loading}
+          size={30}
+          aria-label="Loading Spinner"
+          data-testid="loader"
+        />
+      ) : (
+        <div className="col-span-5 px-10">
+          <h1 className="text-3xl font-semibold text-center mb-5">Orders</h1>
+          <div className="flex justify-around items-center mb-5 bg-slate-300 px-3 rounded-3xl py-3 font-semibold">
+            <span>Order ID</span>
+            <h1>Status</h1>
+            <p>Item Name</p>
+            <p>Amount</p>
+            <p>Actions</p>
+          </div>
 
-        {orders &&
-          orders.map((order) => <OrderData order={order} key={order._id} />)}
-      </div>
+          {orders &&
+            orders.map((order) => <OrderData order={order} key={order._id} />)}
+        </div>
+      )}
     </div>
   );
 };
